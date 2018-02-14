7948cf22522349d16dec851f268724e918cbc837
Michael Cao <mike.x.cao@gmail.com>
2018-02-13 19:57:40 -0500

    boilerplate bot

---
+++ events/ready.js
@@ -0,0 +1,4 @@
+module.exports = async client => {
+  await client.wait(1000);
+  client.logger.log(`[READY] ${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
+};
