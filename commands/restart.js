0e6334273e6fbc7e2d8f1cd5604332ca07a39806
Michael Cao <mike.x.cao@gmail.com>
2018-02-23 20:11:58 -0500

    racer command

---
+++ commands/restart.js
@@ -0,0 +1,21 @@
+exports.run = async (bot, msg, args, level) => {// eslint-disable-line no-unused-vars
+  await msg.reply('bot is restarting...');
+  bot.commands.forEach( async cmd => {
+    await bot.unloadCommand(cmd);
+  });
+  process.exit(1);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'Bot Admin'
+};
+
+exports.help = {
+  name: 'restart',
+  category: 'System',
+  description: 'Shuts down the bot. If running under PM2, bot will restart automatically.',
+  usage: 'restart'
+};
