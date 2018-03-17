55da82278aa7dbc5a08f919f3f16883462d86001
Michael Cao <mike.x.cao@gmail.com>
2018-03-17 19:37:27 -0400

    events and commands

---
+++ commands/gamelist.js
@@ -0,0 +1,17 @@
+exports.run = async (bot, msg, args, level) => { // eslint-disable-line no-unused-vars
+  msg.channel.send('http://arcadep.it/racing/games.html');
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'User'
+};
+
+exports.help = {
+  name: 'gamelist',
+  category: 'Racing',
+  description: 'Returns a link to the game list.',
+  usage: 'gamelist'
+};
---
+++ commands/site.js
@@ -0,0 +1,17 @@
+exports.run = async (bot, msg, args, level) => { // eslint-disable-line no-unused-vars
+  msg.channel.send('http://arcadep.it/racing/index.html');
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'User'
+};
+
+exports.help = {
+  name: 'site',
+  category: 'Racing',
+  description: 'Returns a link to the racing records.',
+  usage: 'site'
+};
