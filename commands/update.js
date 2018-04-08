cc8a78448fe842669f405a6585d43039c51f426d
Michael Cao <mike.x.cao@gmail.com>
2018-04-08 09:16:44 -0400

    update

---
+++ commands/update.js
@@ -0,0 +1,29 @@
+exports.run = async (bot, msg, args, level) => { // eslint-disable-line no-unused-vars
+  msg.channel.send('Updating...').then(e => {
+    var evaled = require('child_process').execSync('git pull').toString();
+    if (evaled.indexOf('Already up-to-date.') > -1) {
+      e.channel.send('There was nothing to update!');
+    } else {
+      e.channel.send('```' + evaled + '```');
+      e.channel.send('New code successfully pulled! Restarting...');
+      setTimeout(() => {
+        process.exit(0);
+      }, 2000);
+    }
+  });
+  return null;
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'Bot Owner'
+};
+
+exports.help = {
+  name: 'update',
+  category: 'System',
+  description: 'Pulls code from Github and restarts.',
+  usage: 'update'
+};
