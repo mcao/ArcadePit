2d5ae1861ebe0d2c5eca65f8f97fe00114109246
Michael Cao <mike.x.cao@gmail.com>
2018-06-15 22:43:01 -0400

    name pls

--- modules/functions.js
+++ modules/functions.js
@@ -27,48 +27,50 @@ module.exports = (bot) => {
       for (var i = 0; i < events.length; i++) {
         var date = new Date(events[i].time),
           timeAway = date - new Date();
-        
+
         if (timeAway < 86400000 && events[i].lastReminderSent < 1) {
           sendReminder(events[i].name, date);
           await bot.database.Events.update({
             lastReminderSent: 1
           }, {
-            where: { id: events[i].id }
-          });
+              where: { id: events[i].id }
+            });
         } if (timeAway < 3600000 && events[i].lastReminderSent < 2) {
           sendReminder(events[i].name, date);
           await bot.database.Events.update({
             lastReminderSent: 2
           }, {
-            where: { id: events[i].id }
-          });
+              where: { id: events[i].id }
+            });
         } if (timeAway < 1800000 && !events[i].open) {
           bot.channels.get(bot.config.raceChannel).send(`<@&380910598742999050>: You may now set yourself as ready for **${events[i].name}**!`);
           await bot.database.Events.update({
             open: true
           }, {
-            where: { id: events[i].id }
-          });
+              where: { id: events[i].id }
+            });
         } if (timeAway < 600000 && events[i].lastReminderSent < 3) {
           sendReminder(events[i].name, date);
           await bot.database.Events.update({
             lastReminderSent: 3
           }, {
-            where: { id: events[i].id }
-          });
+              where: { id: events[i].id }
+            });
         } if (timeAway < 300000 && events[i].lastReminderSent < 4) {
           sendReminder(events[i].name, date);
           await bot.database.Events.update({
             lastReminderSent: 4
           }, {
-            where: { id: events[i].id }
-          });
+              where: { id: events[i].id }
+            });
           setTimeout(bot.startEvent(bot, events[i]), timeAway);
-        } 
+        }
       }
     }
     await checkForEvent(bot);
-    setInterval(await checkForEvent(bot), 30000);
+    setInterval(function () {
+      await checkForEvent(bot);
+    }, 30000);
 
     function sendReminder(event, date) {
       bot.channels.get(bot.config.raceChannel).send(`<@&380910598742999050>: **${event}** is starting **${moment(new Date(date).toISOString()).fromNow()}!**`);
@@ -158,13 +160,13 @@ module.exports = (bot) => {
 
   // <String>.toPropercase() returns a proper-cased string such as: 
   // "Mary had a little lamb".toProperCase() returns "Mary Had A Little Lamb"
-  String.prototype.toProperCase = function() {
-    return this.replace(/([^\W_]+[^\s-]*) */g, function(txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+  String.prototype.toProperCase = function () {
+    return this.replace(/([^\W_]+[^\s-]*) */g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
   };
 
   // <Array>.random() returns a single random element from an array
   // [1, 2, 3, 4, 5].random() can return 1, 2, 3, 4 or 5.
-  Array.prototype.random = function() {
+  Array.prototype.random = function () {
     return this[Math.floor(Math.random() * this.length)];
   };
 
