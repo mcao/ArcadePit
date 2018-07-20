60196d5e77fb228630a7c03bc0fd49587776c6a1
Michael Cao <mike.x.cao@gmail.com>
2018-07-20 19:35:55 -0400

    remove

--- commands/remove.js
+++ commands/remove.js
@@ -1,6 +1,6 @@
 exports.run = async (bot, msg, args, level) => { // eslint-disable-line no-unused-vars
   if (!msg.member.roles.get('380910598742999050')) return msg.reply('You must mention someone to remove!');
-  args.pop()
+  args.splice(0, 1);
   console.log(args.join(' '));
   msg.reply(await bot.database.remove(msg.mentions.users.array()[0], args.join(' ')));
 };
