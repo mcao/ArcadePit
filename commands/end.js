3cee1d354a0df2f652c929d567334f95a40d4afa
Michael Cao <mike.x.cao@gmail.com>
2018-07-21 13:32:45 -0400

    end event

---
+++ commands/end.js
@@ -0,0 +1,36 @@
+exports.run = async (bot, msg, args, level) => { // eslint-disable-line no-unused-vars
+  if (bot.eventInProgress || !bot.event) return msg.channel.send('No event appears to be in progress!');
+  else {
+    var players = bot.event.participants;
+    for (var id in players) {
+      if (bot.event.timed) {
+        if (!players[id].finished)
+          players[id].time = -1;
+      } else {
+        if (!players[id].finished)
+          players[id].score = -1;
+      }
+    }
+    bot.eventInProgress = false;
+    await bot.database.Events.update(bot.event, {
+      where: { id: bot.event.id }
+    });
+    return msg.channel.send('The ' + bot.event.name + ' event has ended!');
+    bot.sendStandings(bot.event.id)
+    bot.event = null;
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'Moderator'
+};
+
+exports.help = {
+  name: 'end',
+  category: 'Racing',
+  description: 'Ends an event early.',
+  usage: 'end'
+};
