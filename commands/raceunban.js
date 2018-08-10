9e2f73cb0633da4d9f5200e300e6e0bdf1c34f84
Michael Cao <mike.x.cao@gmail.com>
2018-08-10 17:56:16 -0400

    unblacklist??

---
+++ commands/raceunban.js
@@ -0,0 +1,32 @@
+exports.run = async (bot, msg, args, level) => { // eslint-disable-line no-unused-vars
+  if (!msg.mentions.users.first()) return msg.reply('please mention someone to unblacklist!');
+  let id = msg.mentions.users.first().id,
+    i = 0,
+    blacklist = require('../banned.json'),
+    found = false;
+  while (i < blacklist.length && !found) {
+    if (blacklist[i] === id) {
+      found = true;
+    }
+    i++;
+  }
+  if (found) {
+    blacklist.splice(i - 1, 1);
+    require('fs').writeFileSync('./banned.json', JSON.stringify(blacklist, null, 3));
+    msg.channel.send('User ' + msg.mentions.users.first() + ' has been unblacklisted!');
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 'Moderator'
+};
+
+exports.help = {
+  name: 'raceunban',
+  category: 'Racing',
+  description: 'Unbans a user from participating in races.',
+  usage: 'raceban'
+};
