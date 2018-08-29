b1937b4dadb1d5146048daec69b047021378b15c
Michael Cao <mike.x.cao@gmail.com>
2018-08-28 23:42:29 -0400

    RaceError class

--- util/RaceError.js
+++ util/RaceError.js
@@ -2,5 +2,8 @@ class RaceError extends Error {
   constructor(message = "", ...args) {
     super(message, ...args);
     this.message = 'RaceError: ' + message;
+    this.toString = function() {
+      return this.message;
+    }
   }
 }
